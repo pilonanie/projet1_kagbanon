1b5f76aead58a66a6ccdfc0b34f50bd20e59f968
pilonanie <pilonanie306@gmail.com>
2024-12-06 13:02:51 +0000

    maj

--- project/src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/auth.controller');
-
-router.get('/login', AuthController.loginPage);
-router.post('/login', AuthController.login);
-router.get('/logout', AuthController.logout);
-
-module.exports = router;
